ee0818a42f6ea2fa67630b07686a5c005b8d7ea7
liangfei <103125590+anhuikylin@users.noreply.github.com>
2026-09-23 15:32:05 +0800

    Add compact ProtVis startup progress bar

--- R/run_app.R
+++ R/run_app.R
@@ -4,6 +4,7 @@
 #' @param options Named list of values passed to `shiny::shinyOptions`.
 #' @param enableBookmarking Can be "url", "server", or "disable".
 #' @param uiPattern A regular expression used to determine which requests should be handled by the UI.
+#' @param progress Logical; show a compact console progress bar while ProtVis initializes.
 #' @param ... Arguments to pass to `golem_opts`. See `?golem::get_golem_options` for more details.
 #' @return An object that represents the app.
 #' @import shiny
@@ -17,23 +18,68 @@ run_ProtVis <- function(
     options = list(),
     enableBookmarking = NULL,
     uiPattern = "/",
+    progress = getOption("protvis.startup.progress", TRUE),
     ...
 ) {
+
+  show_progress <- isTRUE(progress) && interactive()
+  startup_bar <- NULL
+
+  if (show_progress) {
+    base::cat("ProtVis startup ")
+    startup_bar <- utils::txtProgressBar(
+      min = 0,
+      max = 3,
+      initial = 0,
+      style = 3,
+      width = 28
+    )
+    on.exit(
+      if (!is.null(startup_bar)) {
+        base::close(startup_bar)
+      },
+      add = TRUE
+    )
+  }
+
+  advance_startup <- function(step) {
+    if (!is.null(startup_bar)) {
+      utils::setTxtProgressBar(startup_bar, step)
+    }
+    invisible(step)
+  }
+
+  # Step 1: validate the local companion-data installation before building UI.
+  ProtVisDatabase::protvis_database_path(
+    "extdata",
+    "manifest.tsv",
+    must_work = TRUE
+  )
+  advance_startup(1)
 
   # Set a generous default for large proteomics/background workbooks.
   # modifyList ensures that user-defined options are preserved
   default_options <- list(shiny.maxRequestSize = 2 * 1024^3)
   combined_options <- utils::modifyList(default_options, options)
+  advance_startup(2)
+
+  app <- shiny::shinyApp(
+    ui = app_ui,
+    server = app_server,
+    onStart = onStart,
+    options = combined_options,
+    enableBookmarking = enableBookmarking,
+    uiPattern = uiPattern
+  )
+
+  advance_startup(3)
+  if (!is.null(startup_bar)) {
+    base::close(startup_bar)
+    startup_bar <- NULL
+  }
 
   golem::with_golem_options(
-    app = shiny::shinyApp(
-      ui = app_ui,
-      server = app_server,
-      onStart = onStart,
-      options = combined_options,
-      enableBookmarking = enableBookmarking,
-      uiPattern = uiPattern
-    ),
+    app = app,
     golem_opts = list(...)
   )
 }
